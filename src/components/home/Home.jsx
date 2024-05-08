223544c5c8dbea1eb2a11201c78d30784a9aafa7
vb <68516521+cm-verbose@users.noreply.github.com>
2024-05-07 22:25:44 -0400

    major changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,43 +1,37 @@
-import Homepic from '../../assets/images/picthing.png';
+import Homepic from "../../assets/images/png/homebackground.png";
+import Header from "../Header/Header";
 
-import './Home.css';
+import "./Home.css";
 
+export default function Home() {
+  return (
+    <div class="home">
+      <Header
+        links={[
+          { name: "Avatars", loc: "Avatars" },
+          { name: "Cart", loc: "Cart" },
+        ]}
+      ></Header>
+      <div>
+        <img src={Homepic} alt="Home of Linxonix" className="container" />
+        <figcaption>Come and join us!</figcaption>
+        <br />
+        <br />
 
-export default function Home(){
-    return(
-        <div class='home'>
-        <header>
-        <h2 >Linxonix</h2>
-        <div>
-        <nav>
-            <ul>
-            <li key="mnu_0">
-                <a href={`/Avatars`}>Avatars</a>
-              </li>
-                <li>Shopping Cart</li>            
-            </ul>
-        </nav>
+        <div className="box">
+          <h1>About Us!</h1>
+          <p>
+            Linxonix will be the number one in making the community VR chat have a more enjoyable experience while
+            browsing and shopping online.
+          </p>
         </div>
-        </header>
-        <body>
-            <img src={Homepic} alt="Home picture of Linxonix" className='container'/>
-            <figcaption>Come and join us!</figcaption>
-            <br/>
-            <br/>
-           
-            <div className='box'>
-            <h1>About Us!</h1>
-            <p>Linxonix will be the number one in making the community VR 
-                chat have a more enjoyable experience while browsing and shopping online.</p>
-            </div>
-            <br/>
-        </body>
-        <footer>
-            <small>
-                Copyright &copy; Linxonix
-                Contact us at <a href="Linxonix@hotmail.com">Linxonix@gmail.com</a>
-                </small>
-        </footer>
-        </div>
-    )
+        <br />
+      </div>
+      <footer>
+        <small>
+          Copyright &copy; Linxonix Contact us at <a href="Linxonix@hotmail.com">Linxonix@gmail.com</a>
+        </small>
+      </footer>
+    </div>
+  );
 }
